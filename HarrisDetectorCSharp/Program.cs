634a7bcfbb28131b803123e39b6a6a4ef5dadb65
DeppMeng <depp_meng@bitbucket.org>
2018-10-23 21:51:27 +0800

    basic util functions

---
+++ HarrisDetectorCSharp/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace HarrisDetectorCSharp
+{
+    class Program
+    {
+        static Bitmap image;
+        static int[] SobelKernelX = { 1, 0, -1, 2, 0, -2, 1, 0, -1 };
+        static int[] SobelKernelY = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
+
+
+        static void Main(string[] args)
+        {
+            string str = "C:/Depp Data/Others/Wallpaper/Lord of the Ring/1.jpg";
+            GetImage(str);
+            //RGB2Grey(image);
+            List<int[,]> test = GetBGRList(image);
+            VisualizeBGRList(test);
+        }
+
+        static void GetImage(string img)
+        {
+            try
+            {
+                image = new Bitmap(img);
+                return;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Image '{0} not found.", img);
+                return;
+            }
+        }
+
+        static void RGB2Grey(Bitmap bitmap)
+        {
+            int iWidth = bitmap.Width;
+            int iHeight = bitmap.Height;
+            Rectangle rect = new Rectangle(0, 0, iWidth, iHeight);
+            BitmapData bmpData = bitmap.LockBits(rect,
+                ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            IntPtr iPtr = bmpData.Scan0;
+            int iBytes = iWidth * iHeight * 3;
+            byte[] PixelValues = new byte[iBytes];
+            byte[] newPixelValues = new byte[PixelValues.Length];
+            System.Runtime.InteropServices.Marshal.Copy(iPtr, PixelValues, 0, iBytes);
+
+            bitmap.UnlockBits(bmpData);
+            int[,] R = new int[iHeight, iWidth];
+            int[,] G = new int[iHeight, iWidth];
+            int[,] B = new int[iHeight, iWidth];
+            int[,] Grey = new int[iHeight, iWidth];
+
+            int iPoint = 0;
+
+            for (int i = 0; i < iHeight; i++)
+            {
+                for (int j = 0; j < iWidth; j++)
+                {
+                    B[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    G[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    R[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    newPixelValues[iPoint - 3] = Convert.ToByte((B[i, j] + G[i, j] + R[i, j]) / 3);
+                    newPixelValues[iPoint - 2] = Convert.ToByte((B[i, j] + G[i, j] + R[i, j]) / 3);
+                    newPixelValues[iPoint - 1] = Convert.ToByte((B[i, j] + G[i, j] + R[i, j]) / 3);
+                }
+            }
+
+
+            Bitmap Result = new Bitmap(iWidth, iHeight,
+            PixelFormat.Format24bppRgb);
+            Rectangle newrect = new Rectangle(0, 0, iWidth, iHeight);
+            BitmapData newbmpData = Result.LockBits(newrect,
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            IntPtr newiPtr = newbmpData.Scan0;
+            System.Runtime.InteropServices.Marshal.Copy(newPixelValues, 0, newiPtr, PixelValues.Length);
+            Result.Save("C:/Depp Data/Others/Wallpaper/Lord of the Ring/1_grey.jpg", ImageFormat.Jpeg);
+        }
+
+        static List<int[,]> GetBGRList(Bitmap bitmap)
+        {
+            int iWidth = bitmap.Width;
+            int iHeight = bitmap.Height;
+            Rectangle rect = new Rectangle(0, 0, iWidth, iHeight);
+            BitmapData bmpData = bitmap.LockBits(rect,
+                ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            IntPtr iPtr = bmpData.Scan0;
+            int iBytes = iWidth * iHeight * 3;
+            byte[] PixelValues = new byte[iBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(iPtr, PixelValues, 0, iBytes);
+
+            bitmap.UnlockBits(bmpData);
+            int[,] R = new int[iHeight, iWidth];
+            int[,] G = new int[iHeight, iWidth];
+            int[,] B = new int[iHeight, iWidth];
+
+            int iPoint = 0;
+
+            for (int i = 0; i < iHeight; i++)
+            {
+                for (int j = 0; j < iWidth; j++)
+                {
+                    B[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    G[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                    R[i, j] = Convert.ToInt32(PixelValues[iPoint++]);
+                }
+            }
+
+            List<int[,]> result = new List<int[,]>();
+            result.Add(B);
+            result.Add(G);
+            result.Add(R);
+            return result;
+        }
+
+        static void VisualizeBGRList(List<int[,]> list)
+        {
+            int[,] B = list[0];
+            int[,] G = list[1];
+            int[,] R = list[2];
+            int iHeight = B.GetLength(0);
+            int iWidth = B.GetLength(1);
+
+        }
+    }
+}
