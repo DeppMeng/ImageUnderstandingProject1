c538ba890bc750cff6e838d14c5d12636949d1f1
DeppMeng <depp_meng@bitbucket.org>
2018-10-26 22:24:52 +0800

    finished normalize and gradient function

--- HarrisDetectorCSharp/Program.cs
+++ HarrisDetectorCSharp/Program.cs
@@ -8,14 +8,19 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing.Imaging;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace HarrisDetectorCSharp
 {
     class Program
     {
         static Bitmap image;
-        static int[] SobelKernelX = { 1, 0, -1, 2, 0, -2, 1, 0, -1 };
-        static int[] SobelKernelY = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
+        static int[] GradientKernel = { -2, -1, 0, 1, 2 };
+        private class LocationTuple
+        {
+            private int x, y;
+        }
 
 
         static void Main(string[] args)
@@ -24,7 +29,12 @@ namespace HarrisDetectorCSharp
             GetImage(str);
             //RGB2Grey(image);
             List<int[,]> test = GetBGRList(image);
-            VisualizeBGRList(test, "test1");
+            //VisualizeBGRList(test, "test1");
+            int[,] gradientX = new int[1000, 1600];
+            int[,] gradientY = new int[1000, 1600];
+            Tuple<int[,], int[,]> gradient = GetGradient(test[0]);
+            VisualizeGreyImage(Normalize(gradient.Item1), "gradientx");
+            VisualizeGreyImage(Normalize(gradient.Item2), "gradienty");
         }
 
         static void GetImage(string img)
@@ -152,7 +162,94 @@ namespace HarrisDetectorCSharp
                 ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
             IntPtr newiPtr = newbmpData.Scan0;
             System.Runtime.InteropServices.Marshal.Copy(PixelValues, 0, newiPtr, PixelValues.Length);
-            bitmap.Save("C:/Depp Data/Others/Wallpaper/Lord of the Ring/"+name+".jpg", ImageFormat.Jpeg);
+            bitmap.Save("C:/Depp Data/Others/Wallpaper/Lord of the Ring/" + name + ".jpg", ImageFormat.Jpeg);
+        }
+
+        static void VisualizeGreyImage(int[,] list, string name)
+        {
+            int[,] B = list;
+            int[,] G = list;
+            int[,] R = list;
+            int iHeight = B.GetLength(0);
+            int iWidth = B.GetLength(1);
+            byte[] PixelValues = new byte[iWidth * iHeight * 3];
+
+            int iPoint = 0;
+
+            for (int i = 0; i < iHeight; i++)
+            {
+                for (int j = 0; j < iWidth; j++)
+                {
+                    PixelValues[iPoint++] = Convert.ToByte(B[i, j]);
+                    PixelValues[iPoint++] = Convert.ToByte(G[i, j]);
+                    PixelValues[iPoint++] = Convert.ToByte(R[i, j]);
+                }
+            }
+
+            Bitmap bitmap = new Bitmap(iWidth, iHeight, PixelFormat.Format24bppRgb);
+            Rectangle newrect = new Rectangle(0, 0, iWidth, iHeight);
+            BitmapData newbmpData = bitmap.LockBits(newrect,
+                ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            IntPtr newiPtr = newbmpData.Scan0;
+            System.Runtime.InteropServices.Marshal.Copy(PixelValues, 0, newiPtr, PixelValues.Length);
+            bitmap.Save("C:/Depp Data/Others/Wallpaper/Lord of the Ring/" + name + ".jpg", ImageFormat.Jpeg);
+        }
+
+        //static List<LocationTuple> HarrisCornerDetector(List<int[,]> img)
+        //{
+        //    int iHeight = img[0].GetLength(0);
+        //    int iWidth = img[0].GetLength(1);
+        //    var B = img[0];
+        //    var G = img[1];
+        //    var R = img[2];
+        //    var gradientBX = new int[iHeight, iWidth];
+        //    var gradientBY = new int[iHeight, iWidth];
+        //    var gradientGX = new int[iHeight, iWidth];
+        //    var gradientGY = new int[iHeight, iWidth];
+        //    var gradientRX = new int[iHeight, iWidth];
+        //    var gradientRY = new int[iHeight, iWidth];
+        //    List<int[,]> gradient_list = new List<int[,]>();
+        //}
+
+        static Tuple<int[,], int[,]> GetGradient(int[,] img)
+        {
+            int iHeight = img.GetLength(0);
+            int iWidth = img.GetLength(1);
+            var gradientX = new int[iHeight, iWidth];
+            var gradientY = new int[iHeight, iWidth];
+            for (int i = 0; i < iHeight; i++)
+                for (int j = 0; j < iWidth; j++)
+                {
+                    gradientX[i, j] = GradientKernel[0] * img[i, ((j - 2) > 0) ? j - 2 : 0] + GradientKernel[1] * img[i, ((j - 1) > 0) ? j - 1 : 0]
+                        + GradientKernel[2] * img[i, j] + GradientKernel[3] * img[i, ((j + 1) < iWidth) ? j + 1 : iWidth - 1]
+                        + GradientKernel[4] * img[i, ((j + 2) < iWidth) ? j + 2 : iWidth - 1];
+                    gradientY[i, j] = GradientKernel[0] * img[((i - 2) > 0) ? i - 2 : 0, j] + GradientKernel[1] * img[((i - 1) > 0) ? i - 1 : 0, j]
+                        + GradientKernel[2] * img[i, j] + GradientKernel[3] * img[((i + 1) < iHeight) ? i + 1 : iHeight - 1, j]
+                        + GradientKernel[4] * img[((i + 2) < iHeight) ? i + 2 : iHeight - 1, j];
+                }
+            return Tuple.Create(gradientX, gradientY);
+
+        }
+
+        static int[,] Normalize(int[,] img)
+        {
+            double[,] temp_img = new double[img.GetLength(0), img.GetLength(1)];
+            int[,] normalized_img = new int[img.GetLength(0), img.GetLength(1)];
+            double max = img.Cast<int>().Max();
+            double min = img.Cast<int>().Min();
+            for (int i = 0; i < img.GetLength(0); i++)
+                for (int j = 0; j < img.GetLength(1); j++)
+                {
+                    temp_img[i, j] = Math.Log10((img[i, j] - min) + 1);
+                }
+            max = temp_img.Cast<double>().Max();
+            min = temp_img.Cast<double>().Min();
+            for (int i = 0; i < img.GetLength(0); i++)
+                for (int j = 0; j < img.GetLength(1); j++)
+                {
+                    normalized_img[i, j] = Convert.ToInt32((temp_img[i, j] - min) * 255 / ( max - min));
+                }
+            return normalized_img;
         }
     }
 }
